74b878e332fe3f3d728304e179592fb57ebc7f78
Bolaji Azeez <traistapenu@gmail.com>
2020-07-23 19:47:52 +0100

    correctly reference store

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Route, Switch } from 'react-router';
-import store from './store';
+import store from './redux/store';
 import './styles/App.css';
 import { routesArray } from './routes';
 
